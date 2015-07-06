d66f20bc26b4bfe0d53cce24b5942cab951fa15c
ndruha <ndruha@gmail.com>
2015-07-06 18:35:17 +0200

    Context mono behavior fixed.

--- mindi/src/unity/objects/ContextMonoBehaviour.cs
+++ mindi/src/unity/objects/ContextMonoBehaviour.cs
@@ -10,26 +10,8 @@ using minioc.MinDI;
 namespace MinDI.Objects {
 
 	/// <summary>
-	/// Context object is an automatical memory of context on the object.
-	/// Derive your MinDI objects from this class if you don't want to loose the context in some situations
-	/// At the same time, this makes context inaccessible to an ordinary object. 
-	/// If you want an object with context access use PublicContextObject instead. Usually it's a factory that should have such a privilegy.
+	/// Derive your MonoBehaviours that work with MinDI from this class
 	/// </summary>
-	public class ContextMonoBehaviour : DIStateMonoBehaviour
-	{
-	#pragma warning disable 414
-		[NonSerialized]
-		private MiniocContext _context;
-	#pragma warning restore 414
-
-		[InjectionProperty]
-		public MiniocContext __context {
-			set {
-				if (_context != null) {
-					throw new MindiException("Attempt to alter context on ContextObject");
-				}
-				_context = value;
-			}
-		}
+	public class ContextMonoBehaviour : DIStateMonoBehaviour {
 	}
 }
