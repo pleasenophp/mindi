d3b64e631333d0f93272d11af4bf88d2f88196a4
at <at@seriousgames.net>
2015-08-10 10:18:25 +0200

    Added binding name.

---
+++ mindi-interop/helpers/BindingName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MinDI {
+	public static class BindingName {
+		public static string ForType<T>() {
+			return typeof(T).FullName;
+		}
+
+		public static string ForType(object instance) {
+			return instance.GetType().FullName;
+		}
+	}
+}
+
