7f6329415cf4b2959251787999b332a166b37002
at <at@seriousgames.net>
2015-09-01 11:08:25 +0200

    Added running multiple coroutines.

--- mindi-unity/Interfaces/ICoroutineManager.cs
+++ mindi-unity/Interfaces/ICoroutineManager.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using MinDI;
+using System;
 
 namespace MinDI {
 
 	public interface ICoroutineManager {
-
+		// Start one coroutine
 		Coroutine StartCoroutine(IEnumerator routine);
+
+		// Run several coroutines one by one
+		Coroutine StartCoroutines(params IEnumerator[] routines);
+		Coroutine StartCoroutines(Action finalCall, params IEnumerator[] routines);
+
 		void StopCoroutine(IEnumerator routine);
 		void StopCoroutine(Coroutine routine);
 	}
--- mindi-unity/src/core/CoroutineManager.cs
+++ mindi-unity/src/core/CoroutineManager.cs
@@ -1,10 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using MinDI;
+using System;
 
 namespace MinDI.Unity {
 
 	public class CoroutineManager : ContextMonoBehaviour, ICoroutineManager {
+
+		public Coroutine StartCoroutines(params IEnumerator[] routines) {
+			return StartCoroutines(null, routines);
+		}
+
+		public Coroutine StartCoroutines(Action finalCall, params IEnumerator[] routines) {
+			return StartCoroutine(RunCoroutines(finalCall, routines));
+		}
+
+		private IEnumerator RunCoroutines(Action finalCall, params IEnumerator[] routines) {
+			foreach (IEnumerator routine in routines) {
+				yield return StartCoroutine(routine);
+			}
+			if (finalCall != null) {
+				finalCall();
+			}
+			yield break;
+		}
+
 	}
 
 }
