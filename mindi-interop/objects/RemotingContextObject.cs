983ae50f1af8ac11cd2296a24a5a85241d6efe26
at <at@seriousgames.net>
2016-01-15 16:36:01 +0100

    Added remoting context object.

---
+++ mindi-interop/objects/RemotingContextObject.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using MinDI.StateObjects;
+using MinDI.Introspection;
+
+namespace MinDI {
+
+
+	[Serializable]
+	public abstract class RemotingContextObject : MarshalByRefObject, IDIClosedContext {
+
+		[NonSerialized]
+		private ContextDescriptor _descriptor = new ContextDescriptor();
+
+		[Injection]
+		protected IDIContext contextInjection {
+			set {
+				_descriptor.context = value;
+			}
+		}
+
+		void IDIClosedContext.AfterInjection() {
+			OnInjected();
+		}
+
+		void IDIClosedContext.BeforeFactoryDestruction() {
+			OnDestruction();
+		}
+
+		bool IDIClosedContext.IsValid() {
+			return this._descriptor != null;
+		}
+
+		ContextDescriptor IDIClosedContext.descriptor {
+			get {
+				return _descriptor;
+			}
+		}
+
+		protected virtual void OnInjected() {
+		}
+
+		protected virtual void OnDestruction() {
+		}
+	}
+}
