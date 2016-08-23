dab2dee52931c85c7335bc22078c82b3651f8657
at <at@seriousgames.net>
2016-08-23 11:58:49 +0200

    Added missing regression tests for subjectivity.

--- mindi-tests/Core/FixedBugs.cs
+++ mindi-tests/Core/FixedBugs.cs
@@ -10,11 +10,26 @@ namespace MinDI.Tests
     [Category("Unit tests")]
     internal class FixedBugs
     {
-		interface IApple {
+		interface IApple
+		{
+			IOrange orange { get; }
 		}
 
-		class Apple : IApple {
-			
+		interface IOrange
+		{
+		}
+
+		class Apple : ContextObject, IApple
+		{
+			[Injection] public IOrange orange {get; set; }
+		}
+
+		class Orange1 : IOrange
+		{
+		}
+
+		class Orange2 : IOrange
+		{
 		}
 
 
@@ -23,12 +38,47 @@ namespace MinDI.Tests
 			IDIContext context = ContextHelper.CreateContext();
 
 			context.m().Bind<IApple>(() => new Apple());
+			context.m().Bind<IOrange> (() => new Orange1 ());
 
 			IApple appleInstance = context.Resolve<IApple>(null, null);
 			Assert.IsNotNull(appleInstance);
 
 			appleInstance = context.Resolve<IApple>("");
 			Assert.IsNotNull(appleInstance);
+		}
+
+		[Test]
+		public void TestSTNotSubjective ()
+		{
+			IDIContext context = ContextHelper.CreateContext ();
+			context.m ().Bind<IOrange> (() => new Orange1 ());
+			context.s ().Bind<IApple> (() => new Apple ());
+			IDIContext childContext = context.Reproduce ();
+			childContext.m ().Bind<IOrange> (() => new Orange2 ());
+
+			IApple apple2 = childContext.Resolve<IApple> ();
+			IApple apple1 = context.Resolve<IApple> ();
+
+			Assert.AreSame (apple1, apple2);
+			Assert.IsInstanceOf (typeof (Orange1), apple1.orange);
+			Assert.IsInstanceOf (typeof (Orange1), apple2.orange);
+		}
+
+		[Test]
+		public void TestMPSubjective ()
+		{
+			IDIContext context = ContextHelper.CreateContext ();
+			context.m ().Bind<IOrange> (() => new Orange1 ());
+			context.m ().Bind<IApple> (() => new Apple ());
+			IDIContext childContext = context.Reproduce ();
+			childContext.m ().Bind<IOrange> (() => new Orange2 ());
+
+			IApple apple2 = childContext.Resolve<IApple> ();
+			IApple apple1 = context.Resolve<IApple> ();
+
+			Assert.AreNotSame (apple1, apple2);
+			Assert.IsInstanceOf (typeof (Orange1), apple1.orange);
+			Assert.IsInstanceOf (typeof (Orange2), apple2.orange);
 		}
     }
 }
