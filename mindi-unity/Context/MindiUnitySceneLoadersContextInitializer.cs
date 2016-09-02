d72a40a9f4414865fc88d7e4658029ad06f0ff5f
at <at@seriousgames.net>
2016-09-02 16:43:03 +0200

    Unity4-specific code.

---
+++ mindi-unity/Context/MindiUnitySceneLoadersContextInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MinDI.StateObjects;
+using MinDI.Unity;
+
+namespace MinDI.Context.Internal {
+	public class MindiUnitySceneLoadersContextInitializer : IApplicationContextInitializer {
+		#region IContextInitializer implementation
+		public void Initialize(IDIContext context) {
+			context.mbs().Bind<ISceneLoader, Unity4SceneLoader> ();
+			context.m().Bind<IAdditiveSceneLoader> (() => new Unity4AdditiveSceneLoader ());
+		}
+		#endregion
+		
+	}
+}
+
