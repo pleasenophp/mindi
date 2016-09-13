0c9d1bab86a7f393527c591000e8f288a2bff55d
at <at@seriousgames.net>
2016-09-13 11:19:11 +0200

    Added before context start.

--- mindi-unity/src/starter/UnityContextStart.cs
+++ mindi-unity/src/starter/UnityContextStart.cs
@@ -9,6 +9,8 @@ namespace MinDI {
 	public class UnityContextStart : MonoBehaviour {
 
 		public IDIContext CreateContext() {
+			BeforeCreateContext();
+
 			IDIContext context = ContextHelper.CreateContext<IGlobalContextInitializer>(null, "global");
 			context = context.Reproduce<IApplicationContextInitializer>("application");
 			context = ChainUserContexts(context);
@@ -17,6 +19,10 @@ namespace MinDI {
 
 		protected virtual IDIContext ChainUserContexts(IDIContext rootContext) {
 			return rootContext;
+		}
+
+		// Add some global initialization, like setting type providers here
+		protected virtual void BeforeCreateContext () {
 		}
 
 		// All the global initialization happens here
