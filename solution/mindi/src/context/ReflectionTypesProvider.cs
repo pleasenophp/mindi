b522ba0c11a358c7034fb7eaa4accc75110c1526
at <at@seriousgames.net>
2017-02-24 17:06:30 +0100

    Fix in the reflection types provider for the WEB_GL.

--- solution/mindi/src/context/ReflectionTypesProvider.cs
+++ solution/mindi/src/context/ReflectionTypesProvider.cs
@@ -2,33 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
 using System.Reflection;
 using MinDI.Context;
 using MinDI.Helpers;
 
-namespace MinDI
-{
-	public class ReflectionTypesProvider : IContextBuilderTypesProvider
-	{
-		#region IContextBuilderTypesProvider implementation
+namespace MinDI {
+	public class ReflectionTypesProvider : IContextBuilderTypesProvider {
+
+
 		public IList<Type> GetTypes() {
-		    PreloadAssemblies();
+			PreloadAssemblies();
 
-		    // Load types
+			// Load types
 			IList<Type> result = new List<Type>();
-		    Assembly executingAssembly = Assembly.GetCallingAssembly();
-		    Assembly callingAssembly = Assembly.GetCallingAssembly();
-		    Assembly entryAssembly = Assembly.GetEntryAssembly();
+			Assembly executingAssembly = Assembly.GetCallingAssembly();
+			Assembly callingAssembly = Assembly.GetCallingAssembly();
+			Assembly entryAssembly = Assembly.GetEntryAssembly();
 
-			foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+			IEnumerable<Assembly> assemblies = GetAssemblies();
+
+			foreach (Assembly assembly in assemblies) {
 				if (executingAssembly != assembly && callingAssembly != assembly && entryAssembly != assembly &&
-					!assembly.IsUnityProjectAssembly() && !assembly.HasContext()) {
+				    !assembly.IsUnityProjectAssembly() && !assembly.HasContext()) {
 					continue;
 				}
 
 				try {
-
 					foreach (Type type in assembly.GetTypes()) {
 						if (type.IsClass && typeof(IContextInitializer).IsAssignableFrom(type)) {
 							result.Add(type);
@@ -36,28 +35,33 @@ namespace MinDI
 					}
 				}
 				catch (Exception ex) {
-					System.Diagnostics.Debug.WriteLine(string.Format("MinDI failed loading assembly {0}. The exception is: {1}", assembly.FullName, ex.Message));
+					System.Diagnostics.Debug.WriteLine("MinDI failed loading assembly {0}. The exception is: {1}", assembly.FullName,
+						ex.Message);
 					var tle = ex as ReflectionTypeLoadException;
-				    if (tle == null) continue;
-				    foreach (Exception le in tle.LoaderExceptions) {
-				        System.Diagnostics.Debug.WriteLine(string.Format("The loader exception occured for {0}: {1}", assembly.FullName, le.Message));
-				    }
+					if (tle == null) continue;
+					foreach (Exception le in tle.LoaderExceptions) {
+						System.Diagnostics.Debug.WriteLine("The loader exception occured for {0}: {1}", assembly.FullName, le.Message);
+					}
 				}
 			}
 
 			return result;
 		}
 
-	    private static void PreloadAssemblies()
-	    {
-	        var files = FileHelper.AllFilesInApplicationFolder().Where(f => f.Extension == ".dll");
-	        foreach (FileInfo f in files)
-	        {
-	            Assembly.LoadFrom(f.FullName);
-	        }
-	    }
+		private IEnumerable<Assembly> GetAssemblies() {
+			return AppDomain.CurrentDomain.GetAssemblies();
+		}
 
-	    #endregion
+		private static void PreloadAssemblies() {
+#if !UNITY_WEBGL
+			IEnumerable<FileInfo> files = FileHelper.AllFilesInApplicationFolder().Where(f => f.Extension == ".dll");
+			foreach (FileInfo f in files) {
+				Assembly.LoadFrom(f.FullName);
+			}
+#endif
+
+		}
+
 
 	}
 
