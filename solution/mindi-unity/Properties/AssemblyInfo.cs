ae749e2a2d3dcc7c27f821b4e4e8204ef7cc4cec
innerbytes <info@innerbytes.com>
2017-03-26 21:14:46 +0300

    Added unity4 in description.

--- solution/mindi-unity/Properties/AssemblyInfo.cs
+++ solution/mindi-unity/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using MinDI;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("mindi-unity")]
+[assembly: AssemblyTitle("mindi-unity-4")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Innerbytes Software")]
